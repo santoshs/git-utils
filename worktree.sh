480557d8ec262c23273c578677154a1500524de2
Santosh Sivaraj <santosh@fossix.org>
2021-04-02 22:18:55 +0530

    Move repolist array into a file
    Signed-off-by: Santosh Sivaraj <santosh@fossix.org>

--- worktree.sh
+++ worktree.sh
@@ -8,25 +8,38 @@
 # show all the matching repositories. If no pattern, everything is
 # shown and can be searched using the fzf frontend.
 
+declare -gA repolist
+
+function read_repolist()
+{
+    while IFS== read -r key value; do
+        repolist[$key]=$value
+    done < ~/.repolist
+}
+
 function wt ()
 {
     output=""
     filter=""
-    declare -A repolist
 
     if [ $# -eq 1 ]; then
         filter="$1"
     fi
 
-    repolist=([kernel]=$HOME/dev/repos/kernels/linux
-              [ndctl]=$HOME/dev/repos/ndctl
-              [skiboot]=$HOME/dev/repos/skiboot
-              [bugalert]=$HOME/dev/gws/src/github.com/fossix/bugalert
-              [kbuild]=$HOME/dev/gws/src/github.com/santoshs/kbuild)
+    if [ "${#repolist[@]}" -eq 0 ]; then
+        read_repolist
+        if [ $? -ne 0 ]; then
+            echo "failed to read ~/.repolist file"
+        fi
+
+    fi
 
     i=1
     for r in "${!repolist[@]}"; do
-        pushd ${repolist[$r]} > /dev/null
+        pushd ${repolist[$r]} > /dev/null 2>&1
+        if [ $? -ne 0 ]; then
+            continue
+        fi
         areas=(`git worktree list | cut -d' ' -f1`)
         for a in ${areas[@]}; do
             d=`basename $a`
